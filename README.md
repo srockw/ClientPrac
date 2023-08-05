bcd39290c9ee895e6aea8f987da28d8c6c8a4f42
srock <136873475+srockw@users.noreply.github.com>
2023-08-04 21:51:36 -0300

    Credit Duckulus and small guide

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # ClientPrac
 
+- Most of the code here was based on a [Freecam Mod for 1.8.9](https://github.com/Duckulus/Freecam-1.8.9) made by Duckulus, thus why some of the mixins are credited to them
+
 Mod that allows to create a client-sided entity to practice parkour around you.
 
+To use this mod setup a keybind in the MC control settings and press it to toggle practice.
+You can change some settings by going to your mod list ingame and clicking on the ClientPrac mod options.
+
 (This works. But I do not know how safe it is to use in servers)
+
--- src/main/java/io/srock/clientprac/mixin/MixinRenderGlobal.java
+++ src/main/java/io/srock/clientprac/mixin/MixinRenderGlobal.java
@@ -1,17 +1,11 @@
 package io.srock.clientprac.mixin;
 
-import io.srock.clientprac.ClientPrac;
-import io.srock.clientprac.config.Config;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Mixin(RenderGlobal.class)
 public class MixinRenderGlobal {
